cd75db0222023b948f2aa55ed3321a69562bec02
Tapan Patel <tapan2930@gmail.com>
2023-10-19 20:50:20 -0400

    fixinf error: key props

--- src/components/questions/questionDots.js
+++ src/components/questions/questionDots.js
@@ -3,7 +3,7 @@
 const QuestionDots = ({presentQuestion, totalQuestion = 5, handleDotPress}) => {
 
     const dots = Array.from({length: totalQuestion}, (_, index) => ( 
-    <span className={ ` ${presentQuestion === index ? "w-6" : "w-4"} cursor-pointer mt-5 inline-block rounded-md h-3 bg-app.yellow mx-2`}>
+    <span key={index} className={ ` ${presentQuestion === index ? "w-6" : "w-4"} cursor-pointer mt-5 inline-block rounded-md h-3 bg-app.yellow mx-2`}>
     </span>))
     return (
         <div>
